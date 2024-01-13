7444291bb0df065f3adbc37025ee1f9e046cef05
Nitin Agarwal <nitin391618@gmail.com>
2024-01-14 03:00:25 +0530

    Setting TailwindCSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,9 @@ import './App.css'
 
 function App() {
   return (
-    <>
+    <div className="block bg-red-200 pl-3 text-bold text-2xl text-white">
       Hello World
-    </>
+    </div>
   )
 }
 
